3649906f1141bd140b581b27ae275a0a4c9713e7
Alexander Lead <a.lead@yandex.ru>
2024-03-07 11:19:00 +0300

    feat(project): added base classes

---
+++ src/main/java/com/herokuappqa/pages/BasePage.java
@@ -0,0 +1,25 @@
+package com.herokuappqa.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class BasePage {
+    WebDriver driver;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+
+    public void click(WebElement element) {
+        element.click();
+    }
+
+    public void type(WebElement element, String text) {
+        if (text != null) {
+            click(element);
+            element.clear();
+            element.sendKeys(text);
+        }
+    }
+}
